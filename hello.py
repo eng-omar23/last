89a8017af594287d6ffad807140a55ba4183aad1
eng-omar23 <omarmohamud531@gmail.com>
2023-06-21 10:44:10 +0300

    all latest practices

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
-print("hello world !")
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# newlist = []
+
+# for x in fruits:
+#   if "i" in x:
+#     newlist.append(x)
+
+# print(newlist)
+# fruits = ["apple", "banana", "cherry"]
+# fruits["banana"]="kiwi"
+# print(fruits)
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
